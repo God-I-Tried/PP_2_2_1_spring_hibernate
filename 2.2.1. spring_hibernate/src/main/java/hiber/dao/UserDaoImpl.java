49cc93c47a15ecaa3e2107774a155742edc23df7
God-I-Tried <zz022@yandex.ru>
2024-06-10 11:42:32 +0300

    Modified 10.06

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImpl.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImpl.java
@@ -10,7 +10,7 @@ import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
-public class UserDaoImp implements UserDao {
+public class UserDaoImpl implements UserDao {
 
     @Autowired
     private SessionFactory sessionFactory;
